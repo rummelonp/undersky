eb1628b18ea21f2100f2009d119625d57439b190
Kazuya Takeshima <mail@mitukiii.jp>
2011-11-29 14:56:49 +0900

    Add instagram token configuration.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Undersky::Application.initialize!
+
+Instagram.configure do |config|
+  config.client_id = '*** client_id ***'
+  config.client_secret = '*** client_secret ***'
+end
