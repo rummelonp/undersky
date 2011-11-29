62b7bcf236007d7361730599403f505a96587046
Kazuya Takeshima <mail@mitukiii.jp>
2011-11-29 15:46:56 +0900

    Add HTML helper methods.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
   def client
     @client ||= Instagram.client
   end
+
+  def photo_tag(photo, size)
+    image = photo.images.send(size.to_sym)
+    image_tag image.url, {
+      alt: caption_text(photo),
+      width: image.width,
+      height: image.height,
+    }
+  end
+
+  def caption_text(photo)
+    photo.caption.text unless photo.caption.blank?
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -6,4 +6,50 @@ describe ApplicationHelper do
     subject { client }
     it { should be_is_a Instagram::Client }
   end
+
+  before do
+    @photo = Hashie::Mash.new({
+      images: {
+        low_resolution: {
+          url: 'http://example.com/low_resolution.jpg',
+          width: 306,
+          height: 306
+        },
+        thumbnail: {
+          url: 'http://example.com/thumbnail.jpg',
+          width: 150,
+          height: 150
+        },
+        standard_resolution: {
+          url: 'http://example.com/standard_resolution.jpg',
+          width: 612,
+          height: 612
+        }
+      },
+      caption: {
+        text: 'caption text'
+      },
+      created_time: '1298678400',
+    })
+  end
+
+  describe :photo_tag do
+    context :low_resolution do
+      subject { photo_tag @photo, :low_resolution }
+      it { should == '<img alt="caption text" height="306" src="http://example.com/low_resolution.jpg" width="306" />' }
+    end
+    context :thumbnail do
+      subject { photo_tag @photo, :thumbnail }
+      it { should == '<img alt="caption text" height="150" src="http://example.com/thumbnail.jpg" width="150" />' }
+    end
+    context :standard_resolution do
+      subject { photo_tag @photo, :standard_resolution }
+      it { should == '<img alt="caption text" height="612" src="http://example.com/standard_resolution.jpg" width="612" />' }
+    end
+  end
+
+  describe :caption_text do
+    subject { caption_text @photo }
+    it { should == 'caption text' }
+  end
 end
