f8478584dcd3dde5931248741e14bdd7f4634b1c
Kazuya Takeshima <mail@mitukiii.jp>
2011-12-01 17:22:11 +0900

    Make authorize actions & add that specs.

--- app/controllers/authorize_controller.rb
+++ app/controllers/authorize_controller.rb
@@ -1,11 +1,37 @@
 class AuthorizeController < ApplicationController
   def authorize
+    session[:redirect_url] = params[:redirect_url]
+    redirect_to Instagram.authorize_url(
+      scope: [:comments, :likes, :relationships].join(' '),
+      redirect_uri: url_for(:access_token)
+    )
   end
 
   def access_token
+    if params.key?(:error)
+      raise Instagram::Error.new params[:error]
+    end
+    response = Instagram.get_access_token(
+      params[:code],
+      redirect_uri: url_for(:access_token)
+    )
+    session[:access_token] = response.access_token
+    session[:user] = response.user
+  rescue Instagram::Error
+    session.delete(:access_token)
+    session.delete(:user)
+  ensure
+    unless session[:redirect_url].blank?
+      redirect_to session.delete(:redirect_url)
+    else
+      redirect_to :index
+    end
   end
 
   def logout
+    session.delete(:access_token)
+    session.delete(:user)
+    redirect_to :index
   end
 
 end
--- spec/controllers/authorize_controller_spec.rb
+++ spec/controllers/authorize_controller_spec.rb
@@ -2,24 +2,79 @@ require 'spec_helper'
 
 describe AuthorizeController do
 
-  describe "GET 'authorize'" do
-    it "returns http success" do
-      get 'authorize'
-      response.should be_success
+  describe 'GET "authorize"' do
+    before do
+      get :authorize, redirect_url: '*** redirect_url ***'
+    end
+
+    it 'should be redirect' do
+      response.should be_redirect
+    end
+
+    it 'session have redirect_url' do
+      session[:redirect_url].should be_present
     end
   end
 
-  describe "GET 'access_token'" do
-    it "returns http success" do
-      get 'access_token'
-      response.should be_success
+  describe 'GET "access_token"' do
+    context "authenticated" do
+      before do
+        session[:redirect_url] = '*** redirect_url ***'
+        @data = Hashie::Mash.new({
+          access_token: '*** access_token ***',
+          user: '*** user ***'
+        })
+        Instagram.should_receive(:get_access_token).and_return(@data)
+        get :access_token
+      end
+
+      it 'should be redirect' do
+        response.should be_redirect
+      end
+
+      it 'should have access_token' do
+        session[:access_token].should be_present
+      end
+
+      it 'should have user' do
+        session[:user].should be_present
+      end
+
+      it 'should not have redirect_url' do
+        session[:redirect_url].should be_blank
+      end
+    end
+
+    context 'have error' do
+      before do
+        get :access_token, error: 'error'
+      end
+
+      it 'should not have access_token' do
+        session[:access_token].should be_blank
+      end
+
+      it 'should not have user' do
+        session[:user].should be_blank
+      end
     end
   end
 
-  describe "GET 'logout'" do
-    it "returns http success" do
-      get 'logout'
-      response.should be_success
+  describe 'GET "logout"' do
+    before do
+      get :logout
+    end
+
+    it 'should be redirect' do
+      response.should be_redirect
+    end
+
+    it 'should not have access_token' do
+      session[:access_token].should be_blank
+    end
+
+    it 'should not have user' do
+      session[:user].should be_blank
     end
   end
 
