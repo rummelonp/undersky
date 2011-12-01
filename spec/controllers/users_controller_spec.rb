a65fa125df579856ebfd5baae2db5e2b9b34dd6c
Kazuya Takeshima <mail@mitukiii.jp>
2011-12-02 00:03:47 +0900

    Add users controller specs.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,31 +1,57 @@
 require 'spec_helper'
 
 describe UsersController do
+  before do
+    @client = Instagram.client
+
+    @photo = photo_response
+    @photos = [@photo] * 10
+    @data = Hashie::Mash.new(data: @photos)
+  end
 
   describe "GET 'feed'" do
-    it "returns http success" do
-      get 'feed'
+    before do
+      Instagram::Client.should_receive(:new).and_return(@client)
+      @client.should_receive(:user_media_feed).and_return(@data)
+      get :feed
+    end
+
+    it 'should be success' do
       response.should be_success
     end
   end
 
   describe "GET 'self'" do
-    it "returns http success" do
-      get 'self'
-      response.should be_success
+    before do
+      session[:user] = {id: 982876}
+      get :self
+    end
+
+    it 'should be redirect' do
+      response.should be_redirect
     end
   end
 
   describe "GET 'recent'" do
-    it "returns http success" do
-      get 'recent'
+    before do
+      Instagram::Client.should_receive(:new).and_return(@client)
+      @client.should_receive(:user_recent_media).and_return(@photos)
+      get :recent, id: 982876
+    end
+
+    it 'should be success' do
       response.should be_success
     end
   end
 
   describe "GET 'liked'" do
-    it "returns http success" do
-      get 'liked'
+    before do
+      Instagram::Client.should_receive(:new).and_return(@client)
+      @client.should_receive(:user_liked_media).and_return(@data)
+      get :liked
+    end
+
+    it 'should be success' do
       response.should be_success
     end
   end
