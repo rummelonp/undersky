7830b0d03cec70f224964c8592d646bff8d6c026
Kazuya Takeshima <mail@mitukiii.jp>
2011-12-01 23:57:22 +0900

    Split photo dummy response to spec helper method.

--- spec/controllers/media_controller_spec.rb
+++ spec/controllers/media_controller_spec.rb
@@ -5,61 +5,7 @@ describe MediaController do
   before do
     @client = Instagram.client
 
-    @photo = Hashie::Mash.new({
-      tags: [],
-      location: nil,
-      comments: {
-        count: 0,
-        data: []
-      },
-      filter: 'filter',
-      created_time: '1298678400',
-      link: 'http://example.com/link.html',
-      likes: {
-        count: 0,
-        data: []
-      },
-      images: {
-        low_resolution: {
-          url: 'http://example.com/low_resolution.jpg',
-          width: 306,
-          height: 306
-        },
-        thumbnail: {
-          url: 'http://example.com/thumbnail.jpg',
-          width: 150,
-          height: 150
-        },
-        standard_resolution: {
-          url: 'http://example.com/standard_resolution.jpg',
-          width: 612,
-          height: 612
-        }
-      },
-      caption: {
-        text: 'caption text'
-      },
-      created_time: '1298678400',
-      caption: {
-        created_time: '1298678400',
-        text: 'caption text',
-        from: {
-          username: 'test user',
-          profile_picture: 'http://example.com/profile_picture.jpg',
-          id: '1',
-          full_name: 'test user'},
-        id: '1'
-      },
-      type: 'image',
-      id: '1',
-      user: {
-        username: 'test user',
-        profile_picture: 'http://example.com/profile_picture.jpg',
-        id: '1',
-        full_name: 'test user'
-      }
-    })
-
+    @photo = photo_response
     @photos = [@photo] * 10
   end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,68 @@ require 'rspec/autorun'
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
+module SpecHelper
+  def photo_response
+    Hashie::Mash.new({
+      tags: [],
+      location: nil,
+      comments: {
+        count: 0,
+        data: []
+      },
+      filter: 'filter',
+      created_time: '1298678400',
+      link: 'http://example.com/link.html',
+      likes: {
+        count: 0,
+        data: []
+      },
+      images: {
+        low_resolution: {
+          url: 'http://example.com/low_resolution.jpg',
+          width: 306,
+          height: 306
+        },
+        thumbnail: {
+          url: 'http://example.com/thumbnail.jpg',
+          width: 150,
+          height: 150
+        },
+        standard_resolution: {
+          url: 'http://example.com/standard_resolution.jpg',
+          width: 612,
+          height: 612
+        }
+      },
+      caption: {
+        text: 'caption text'
+      },
+      created_time: '1298678400',
+      caption: {
+        created_time: '1298678400',
+        text: 'caption text',
+        from: {
+          username: 'test user',
+          profile_picture: 'http://example.com/profile_picture.jpg',
+          id: '1',
+          full_name: 'test user'},
+        id: '1'
+      },
+      type: 'image',
+      id: '1',
+      user: {
+        username: 'test user',
+        profile_picture: 'http://example.com/profile_picture.jpg',
+        id: '1',
+        full_name: 'test user'
+      }
+    })
+  end
+end
+
 RSpec.configure do |config|
+  config.include(SpecHelper)
+
   # == Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
