546b8d9fb2d0ecceaf26bacdb0be9c3a6b09235e
Kazuya Takeshima <mail@mitukiii.jp>
2011-12-01 23:50:38 +0900

    Make users base design.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,21 @@
 class UsersController < ApplicationController
   def feed
+    @data = client.user_media_feed params
+    @photos = @data.data
   end
 
   def self
+    redirect_to recent_url(id: session[:user][:id])
   end
 
   def recent
+    id = params.delete :id
+    @photos = client.user_recent_media id, params
   end
 
   def liked
+    @data = client.user_liked_media params
+    @photos = @data.data
   end
 
 end
