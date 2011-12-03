04ea99ef67dc494fc77ee43884bb51cbee7ffe9a
Kazuya Takeshima <mail@mitukiii.jp>
2011-12-03 22:24:05 +0900

    Add authenticate filter to comments controller.

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,23 +1,46 @@
 require 'spec_helper'
 
 describe CommentsController do
+  context "not authenticated" do
+    describe "GET 'comments'" do
+      it 'should be redirect' do
+        xhr :get, :comments, id: 9578621
+        response.should be_redirect
+      end
+    end
 
-  describe "GET 'comments'" do
-    it "returns http success" do
-      pending "to be implement after"
+    describe "POST 'comments'" do
+      it 'should be redirect' do
+        xhr :post, :create_comment, id: 9578621, text: 'comment'
+        response.should be_redirect
+      end
+    end
+
+    describe "DELETE 'comments'" do
+      it 'should be redirect' do
+        xhr :delete, :delete_comment, id: 9578621
+        response.should be_redirect
+      end
     end
   end
 
-  describe "GET 'create_comment'" do
-    it "returns http success" do
-      pending "to be implement after"
+  context "authenticated" do
+    describe "GET 'comments'" do
+      it "returns http success" do
+        pending "to be implement after"
+      end
+    end
+
+    describe "GET 'create_comment'" do
+      it "returns http success" do
+        pending "to be implement after"
+      end
+    end
+
+    describe "GET 'delete_comment'" do
+      it "returns http success" do
+        pending "to be implement after"
+      end
     end
   end
-
-  describe "GET 'delete_comment'" do
-    it "returns http success" do
-      pending "to be implement after"
-    end
-  end
-
 end
