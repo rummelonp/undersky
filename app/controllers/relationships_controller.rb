7c512037b7170d94fc2ec70b57c2d9a8af3bbece
Kazuya Takeshima <mail@mitukiii.jp>
2011-12-04 13:36:15 +0900

    Make relationships base actions, design & that specs.

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,10 +2,15 @@ class RelationshipsController < ApplicationController
   before_filter :authenticate
 
   def follows
-    
+    id = params.delete(:id)
+    @users = client.user_follows id, params.merge(count: 100)
+    @user = client.user id
   end
 
   def followed_by
+    id = params.delete(:id)
+    @users = client.user_followed_by id, params.merge(count: 100)
+    @user = client.user id
   end
 
 end
--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -16,4 +16,41 @@ describe RelationshipsController do
       end
     end
   end
+
+  context "authenticated" do
+    before do
+      @client = Instagram.client
+
+      @user = user_response
+      @users = [@user] * 100
+
+      session[:access_token] = '*** access token ***'
+    end
+
+    describe "GET 'follows'" do
+      before do
+        Instagram::Client.should_receive(:new).and_return(@client)
+        @client.should_receive(:user_follows).and_return(@users)
+        @client.should_receive(:user).and_return(@user)
+        get :follows, id: 982876
+      end
+
+      it "should be redirect" do
+        response.should be_success
+      end
+    end
+
+    describe "GET 'followed_by'" do
+      before do
+        Instagram::Client.should_receive(:new).and_return(@client)
+        @client.should_receive(:user_followed_by).and_return(@users)
+        @client.should_receive(:user).and_return(@user)
+        get :followed_by, id: 982876
+      end
+
+      it "should be redirect" do
+        response.should be_success
+      end
+    end
+  end
 end
