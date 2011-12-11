2a7e971ac7ea804d5d4b1547d5bbc904d6f52e37
Kazuya Takeshima <mail@mitukiii.jp>
2011-12-11 14:46:10 +0900

    Add google analytics code.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,11 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-18367987-6']);
+_gaq.push(['_trackPageview']);
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
