5b263043655e7951704bffa1fbe2c3c60e2a91ac
Kazuya Takeshima <mail@mitukiii.jp>
2011-12-11 16:26:15 +0900

    Add tags recent action.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,10 @@
 class TagsController < ApplicationController
   def recent
+    name = params.delete(:name)
+    response = client.tag_recent_media name, params.merge(count: 100)
+    @photos = response.data
+    @pagination = response.pagination
+    @tag = client.tag name
   end
 
 end
--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -3,8 +3,19 @@ require 'spec_helper'
 describe TagsController do
 
   describe "GET 'recent'" do
-    it "returns http success" do
-      get 'recent'
+    before do
+      @client = Instagram.client
+      @tag = tag_response
+      @photo = photo_response
+      @photos = [@photo] * 10
+      @data = Hashie::Mash.new(data: @photos)
+      Instagram::Client.should_receive(:new).and_return(@client)
+      @client.should_receive(:tag_recent_media).and_return(@data)
+      @client.should_receive(:tag).and_return(@tag)
+      get :recent, name: 'query'
+    end
+
+    it "should be success" do
       response.should be_success
     end
   end
