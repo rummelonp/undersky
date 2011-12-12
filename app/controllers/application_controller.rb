f7ace264766f1c109a56c91e55ac65d4c47adfa7
Kazuya Takeshima <mail@mitukiii.jp>
2011-12-12 18:39:47 +0900

    Add internal server error & client error handlers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,39 @@ class ApplicationController < ActionController::Base
   def authenticate
     unless authenticated?
       if request.xhr?
-        render text: 'is not authenticated', status: 403
+        raise Instagram::BadRequest.new 'you are not authorized'
       else
         redirect_to authorize_url(redirect_url: request.fullpath)
       end
+    end
+  end
+
+  rescue_from Instagram::Error do |e|
+    logger.info e
+    if request.xhr?
+      render text: e.message, status: 400
+    else
+      @notice = e.message
+      render template: 'error/client', status: 400
+    end
+  end
+
+  rescue_from StandardError do |e|
+    case e
+    when Instagram::Error
+      logger.info e
+      status = 400
+      template = 'error/client'
+    else
+      logger.error e
+      status = 500
+      template = 'error/server'
+    end
+    @message = e.message.split(':')[-2..-1].join(':')
+    if request.xhr?
+      render text: message, status: status
+    else
+      render template: template, status: status
     end
   end
 end
