73dc10990ada2e316e0e3daf37a5b0236b0bc87e
Kazuya Takeshima <mail@mitukiii.jp>
2013-01-13 03:42:09 +0900

    require hashie at initializer that to use hashie at session store.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
+# Use hashie at session store
+require 'hashie'
+
 Undersky::Application.config.session_store :cookie_store, key: '_undersky_session'
 
 # Use the database for sessions instead of the cookie-based default,
